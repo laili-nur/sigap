c00e37fefa18835c20a72310b3d63e5b09b0a0d6
bagaskara <bgsbla33333@gmail.com>
2020-02-26 22:36:35 +0700

    Move data pendukung to modules

--- application/modules/work_unit/controllers/Work_unit.php
+++ application/modules/work_unit/controllers/Work_unit.php
@@ -1,7 +1,9 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Work_unit extends Operator_Controller {
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+class Work_unit extends Operator_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->pages = 'work_unit';
         //khusus admin
@@ -9,25 +11,29 @@ class Work_unit extends Operator_Controller {
         if ($ceklevel == 'author' || $ceklevel == 'reviewer' || $ceklevel == 'editor' || $ceklevel == 'layouter') {
             redirect('home');
         }
+
+        $this->load->model('Work_unit_model', 'work_unit');
     }
 
-    public function index($page = null) {
+    public function index($page = null)
+    {
         $work_units = $this->work_unit->orderBy('work_unit_name')->getAll();
-        $total = count($work_units);
-        $pages = $this->pages;
-        $main_view = 'workunit/index_work_unit';
+        $total      = count($work_units);
+        $pages      = $this->pages;
+        $main_view  = 'work_unit/index_work_unit';
         $this->load->view('template', compact('pages', 'main_view', 'work_units', 'total'));
     }
 
-    public function add() {
+    public function add()
+    {
         if (!$_POST) {
-            $input = (object)$this->work_unit->getDefaultValues();
+            $input = (object) $this->work_unit->getDefaultValues();
         } else {
-            $input = (object)$this->input->post(null, true);
+            $input = (object) $this->input->post(null, true);
         }
         if (!$this->work_unit->validate()) {
-            $pages = $this->pages;
-            $main_view = 'workunit/form_work_unit';
+            $pages       = $this->pages;
+            $main_view   = 'workunit/form_work_unit';
             $form_action = 'work_unit/add';
             $this->load->view('template', compact('pages', 'main_view', 'form_action', 'input'));
             return;
@@ -40,20 +46,21 @@ class Work_unit extends Operator_Controller {
         redirect('workunit');
     }
 
-    public function edit($id = null) {
+    public function edit($id = null)
+    {
         $work_unit = $this->work_unit->where('work_unit_id', $id)->get();
         if (!$this) {
             $this->session->set_flashdata('warning', 'Work Unit data were not available');
             redirect('workunit');
         }
         if (!$_POST) {
-            $input = (object)$work_unit;
+            $input = (object) $work_unit;
         } else {
-            $input = (object)$this->input->post(null, true);
+            $input = (object) $this->input->post(null, true);
         }
         if (!$this->work_unit->validate()) {
-            $pages = $this->pages;
-            $main_view = 'workunit/form_work_unit';
+            $pages       = $this->pages;
+            $main_view   = 'workunit/form_work_unit';
             $form_action = "work_unit/edit/$id";
             $this->load->view('template', compact('pages', 'main_view', 'form_action', 'input'));
             return;
@@ -66,7 +73,8 @@ class Work_unit extends Operator_Controller {
         redirect('workunit');
     }
 
-    public function delete($id = null) {
+    public function delete($id = null)
+    {
         $code = $this->work_unit->where('work_unit_id', $id)->get();
         if (!$code) {
             $this->session->set_flashdata('warning', 'Work Unit data were not available');
@@ -79,14 +87,15 @@ class Work_unit extends Operator_Controller {
         }
         redirect('workunit');
     }
-   
-    public function unique_work_unit_name() {
+
+    public function unique_work_unit_name()
+    {
         $work_unit_name = $this->input->post('work_unit_name');
-        $work_unit_id = $this->input->post('work_unit_id');
+        $work_unit_id   = $this->input->post('work_unit_id');
         $this->work_unit->where('work_unit_name', $work_unit_name);
         !$work_unit_id || $this->work_unit->where('work_unit_id !=', $work_unit_id);
         $work_unit = $this->work_unit->get();
-        if (count($work_unit)) {
+        if ($work_unit) {
             $this->form_validation->set_message('unique_work_unit_name', '%s has been used');
             return false;
         }
